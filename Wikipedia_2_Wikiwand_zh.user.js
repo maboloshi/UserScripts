c656540958abb02ac7702dac67f3411ac28b30eb
沙漠之子 <ymc1216@gmail.com>
2019-02-06 13:41:17 +0800

    Update Wikipedia_2_Wikiwand_zh.user.js

--- Wikipedia_2_Wikiwand_zh.user.js
+++ Wikipedia_2_Wikiwand_zh.user.js
@@ -3,10 +3,10 @@
 // @description         Redirect Wikipedia to Wikiwand for a modern browsing experience.
 // @description:zh-CN   重定向 Wikipedia 页面到 Wikiwand 以获得现代的浏览体验
 // @description:zh-TW   重定向 Wikipedia 頁面到 Wikiwand 以獲得現代的瀏覽體驗
-// @version             1.0.0
+// @version             1.0.1
 // @namespace           https://github.com/maboloshi/UserScripts
 // @icon                https://www.wikiwand.com/favicon.ico
-// @include             http*://*.wikipedia.org/wiki/*
+// @include             http*://*.wikipedia.org/*/*
 // @run-at              document-start
 // ==/UserScript==
 window.location.replace(document.location.href.replace(/https?:\/\/([\w-]+)\.wikipedia\.org\/[\w-]+/, 'https://www.wikiwand.com/zh'));
