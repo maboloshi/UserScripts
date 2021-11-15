e6ff1e0cb48da9455cbb7d994481fd07443fec79
沙漠之子 <7850715+maboloshi@users.noreply.github.com>
2021-11-15 10:18:01 +0800

    Update Wikipedia_2_Wikiwand_zh.user.js

--- Wikipedia_2_Wikiwand_zh.user.js
+++ Wikipedia_2_Wikiwand_zh.user.js
@@ -3,7 +3,7 @@
 // @description         Redirect Wikipedia to Wikiwand for a modern browsing experience.
 // @description:zh-CN   重定向 Wikipedia 页面到 Wikiwand 以获得现代的浏览体验
 // @description:zh-TW   重定向 Wikipedia 頁面到 Wikiwand 以獲得現代的瀏覽體驗
-// @version             1.0.1
+// @version             1.0.2
 // @namespace           https://github.com/maboloshi/UserScripts
 // @icon                https://www.wikiwand.com/favicon.ico
 // @include             http*://*.wikipedia.org/*/*
