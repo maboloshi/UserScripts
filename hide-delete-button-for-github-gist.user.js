cd746074c5fe1f384a7e2bac83f5ed2935aa37fa
沙漠之子 <7850715+maboloshi@users.noreply.github.com>
2022-06-14 10:50:59 +0800

    Create hide-delete-button-for-github-gist.user.js

---
+++ hide-delete-button-for-github-gist.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        隐藏 Github Gist 删除按钮
+// @name.en     Hide Delete button for GitHub Gist
+// @version     0.1.0
+// @namespace   https://github.com/maboloshi/UserScripts/
+// @match       https://gist.github.com/*/*
+// @grant       GM_registerMenuCommand
+// @icon        https://github.githubassets.com/pinned-octocat.svg
+// @run-at      document-end
+// ==/UserScript==
+(function () {
+    'use strict';
+  //console.log("==> 载入 Hide Delete button for GitHub Gist");
+
+   var el = document.getElementsByClassName("btn-danger btn-sm btn")[0]
+
+   if (!el) {
+     return;     
+    }
+  el.style.display = 'none';
+
+   // 注册菜单
+    GM_registerMenuCommand("显示/隐藏\"删除按钮\"", Toggle);
+    function Toggle() {
+      if (el.style.display == '') {
+        el.style.display = 'none';
+      } else {
+        el.style.display = '';
+      }      
+    }
+  
+  //console.log("==> 退出 Hide Delete button for GitHub Gist");  
+  
+})();
