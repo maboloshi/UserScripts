b658c422815f40239a95cd1aa5da207f518847d1
沙漠之子 <7850715+maboloshi@users.noreply.github.com>
2023-08-25 13:39:32 +0800

    Update hide-delete-button-for-github-gist.user.js
    代码优化

--- hide-delete-button-for-github-gist.user.js
+++ hide-delete-button-for-github-gist.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            隐藏 Github Gist 删除按钮
 // @name.en         Hide Delete button for GitHub Gist
-// @version         0.1.0
+// @version         0.1.1
 // @description     隐藏 Github Gist 删除按钮
 // @description:en  Hide Delete button for GitHub Gist
 // @namespace       https://github.com/maboloshi/UserScripts/
@@ -11,26 +11,23 @@
 // @run-at          document-end
 // ==/UserScript==
 (function () {
-    'use strict';
-  //console.log("==> 载入 Hide Delete button for GitHub Gist");
+  'use strict';
+  console.log("==> 载入 Hide Delete button for GitHub Gist");
 
-   var el = document.getElementsByClassName("btn-danger btn-sm btn")[0]
+   var el = document.querySelector('.btn-danger.btn-sm.btn');
 
-   if (!el) {
-     return;
+   if (el) {
+     el.style.display = 'none';
     }
-  el.style.display = 'none';
 
    // 注册菜单
-    GM_registerMenuCommand("显示/隐藏\"删除按钮\"", Toggle);
-    function Toggle() {
-      if (el.style.display == '') {
-        el.style.display = 'none';
-      } else {
-        el.style.display = '';
+    GM_registerMenuCommand("显示/隐藏\"删除按钮\"", toggle);
+    function toggle() {
+      if (el) {
+        el.style.display = el.style.display === 'none' ? '' : 'none';
       }
     }
 
-  //console.log("==> 退出 Hide Delete button for GitHub Gist");
+//console.log("==> 退出 Hide Delete button for GitHub Gist");
 
 })();
