4bc8f205e5a290228d5a34671220bb21338e27c8
沙漠之子 <7850715+maboloshi@users.noreply.github.com>
2023-09-27 17:46:48 +0800

    新增`切换展开/折叠按钮位置`功能, 调整按钮样式等, 代码优化
    1. 使用`getElements系列`代替`querySelectorAll`,`querySelector`提高一丢丢性能
    2. 调整按钮样式, 并修改 Raw按钮原有样式
    3. 新增`切换展开/折叠按钮位置`功能, 及默认样式
    4. 调整`deleteBtnToggle`触发机制
    5. 调整缩进

--- Add_Copy_Download_ExpandCollapse_Button_for_github_gist.user.js
+++ Add_Copy_Download_ExpandCollapse_Button_for_github_gist.user.js
@@ -2,7 +2,7 @@
 // @name            Github Gists: 添加复制、下载和展开/折叠文件按钮，隐藏/显示删除按钮
 // @name.en         Github Gists: Add copy, download and expand/collapse file buttons, hide/show delete button
 // @namespace       https://github.com/maboloshi/UserScripts/
-// @version         0.4
+// @version         0.5
 // @description     为 GitHub Gists 添加复制、下载和展开/折叠文件按钮，隐藏/显示删除按钮
 // @description.en  Adds copy, download, expand/collapse file buttons and hide/show delete button for GitHub Gists
 // @author          maboloshi
@@ -10,8 +10,10 @@
 // @grant           GM_addStyle
 // @grant           GM_setClipboard
 // @grant           GM_download
-// @grant           GM_registerMenuCommand
 // @grant           GM_xmlhttpRequest
+// @grant           GM_getValue
+// @grant           GM_setValue
+// @grant           GM_registerMenuCommand
 // @icon            https://github.githubassets.com/pinned-octocat.svg
 // @run-at          document-end
 // ==/UserScript==
@@ -22,44 +24,65 @@
   // 添加样式
   GM_addStyle(`
     .gist-expand-collapse-btn {
-        margin: 0 0 0 6px;
-        display: inline-block;
-        padding: 5px;
-        line-height: 1;
-        color: var(--fgColor-muted, var(--color-fg-muted));
-        vertical-align: middle;
-        background: transparent;
-        border: 0;
-        box-shadow: none;
+      order: 3;
+
+      display: inline-block;
+      box-shadow: none;
+      padding: 5px;
+      border: 0;
+      margin: 0 0 0 6px;
+      vertical-align: middle;
+
+      line-height: 1;
+
+      background: transparent;
+      color: var(--fgColor-muted, var(--color-fg-muted));
     }
-    .gist-expand-collapse-btn:hover {
-        color: var(--fgColor-accent, var(--color-accent-fg));
+    .gist-expand-collapse-btn:hover, .copy-download-btn > svg:hover {
+      color: var(--fgColor-accent, var(--color-accent-fg));
     }
-    .gist-file-box-collapsed, .gist-btn-hide { display: none !important; }
+    .gist-expand-collapse-btn-left {
+      order: unset;
+      margin: 0 6px 0 0;
+    }
+    .gist-file-box-collapsed, .gist-btn-hide {
+      display: none !important;
+    }
+    .file-header.d-flex.flex-md-items-center.flex-items-start {
+      align-items: center !important;
+    }
     .file-actions.flex-order-2.pt-0 {
-        display: inline-flex;
-        align-items: center !important;
+      display: inline-flex;
     }
-    a.Button--secondary.Button--small.Button {
-        border-top-right-radius: 0px;
-        border-bottom-right-radius: 0px;
+    .file-actions.file-actions > :first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+    .file-actions.file-actions > :last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+    .file-actions.file-actions > * {
+      display: inline-grid;
+      place-content: center;
+      height: 28px;
+      margin-inline-end: -1px;
+      border-radius: 0;
+    }
+    .raw-btn {
+      padding: 0 8px;
     }
     .copy-download-btn {
-        width: 28px;
-        display: grid;
-        place-content: center;
-    }
-    button[aria-label="复制原始文件"] {
-        border-radius: 0px;
-    }
-    button[aria-label="下载原始文件"] {
-        border-top-left-radius: 0px;
-        border-bottom-left-radius: 0px;
+      width: 28px;
+      padding: unset;
     }
     Details-content--hidden, Details-content--shown {
-        position: absolute;
+      position: absolute;
     }
-  `);
+    `);
+
+  // 折叠/展开按钮 默认在左侧
+  let allowExpandCollapseBtnLeftFloat = GM_getValue("allowExpandCollapseBtnLeftFloat", true);
 
   // 将文本复制到剪贴板
   function copyToClipboard(el, text) {
@@ -79,12 +102,12 @@
       url: fileUrl,
       name: fileName,
       onload: () => {
-          console.log('✅ 文件下载完成');
-          showNotification(el, '✅ 文件下载完成');
+        console.log('✅ 文件下载完成');
+        showNotification(el, '✅ 文件下载完成');
       },
       onerror: (error) => {
-          console.error('❌ 下载出错:', error);
-          showNotification(el, '❌ 下载出错');
+        console.error('❌ 下载出错:', error);
+        showNotification(el, '❌ 下载出错');
       }
     });
   }
@@ -136,12 +159,19 @@
 
   // 初始化按钮
   function initButtons() {
-    const files = document.querySelectorAll('.file');
+    const files = document.getElementsByClassName('file');
 
-    files.forEach(file => {
-      const fileAction =file.firstElementChild.firstElementChild;
+    [...files].forEach(file => {
+      const fileAction = file.firstElementChild.firstElementChild;
       const fileUrl = fileAction.firstElementChild.href;
       const fileName = file.querySelector('.gist-blob-name').innerText;
+
+      // 修改Raw按钮样式
+      const rawBtn = fileAction.firstElementChild;
+      rawBtn.removeAttribute('class');
+      rawBtn.classList.add('btn', 'raw-btn');
+      rawBtn.firstElementChild.removeAttribute('class');
+      rawBtn.firstElementChild.firstElementChild.removeAttribute('class');
 
       // 创建复制按钮
       const copyBtn = createButton(
@@ -149,9 +179,9 @@
         '复制原始文件',
         `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" class="octicon octicon-copy"><path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path></svg>`,
         'btn', 'copy-download-btn'
-      );
+        );
       copyBtn.addEventListener('click', async () => {
-        const fileBox = file.querySelector('[name="gist[content]"]');
+        const fileBox = file.getElementsByTagName('textarea')[0];
         const fileContent = fileBox?.innerText || await getRawText(fileUrl);
         copyToClipboard(copyBtn, fileContent);
       });
@@ -163,7 +193,7 @@
         '下载原始文件',
         `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" viewBox="0 0 16 16" class="octicon octicon-download"><path d="M2.75 14A1.75 1.75 0 0 1 1 12.25v-2.5a.75.75 0 0 1 1.5 0v2.5c0 .138.112.25.25.25h10.5a.25.25 0 0 0 .25-.25v-2.5a.75.75 0 0 1 1.5 0v2.5A1.75 1.75 0 0 1 13.25 14Z"></path><path d="M7.25 7.689V2a.75.75 0 0 1 1.5 0v5.689l1.97-1.969a.749.749 0 1 1 1.06 1.06l-3.25 3.25a.749.749 0 0 1-1.06 0L4.22 6.78a.749.749 0 1 1 1.06-1.06l1.97 1.969Z"></path></svg>`,
         'btn', 'copy-download-btn',
-      );
+        );
       downloadBtn.addEventListener('click', () => {
         downloadFile(downloadBtn, fileUrl, fileName);
       });
@@ -174,11 +204,11 @@
         '折叠',
         '折叠文件',
         `
-          <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-chevron-down Details-content--hidden"><path d="M12.78 5.22a.749.749 0 0 1 0 1.06l-4.25 4.25a.749.749 0 0 1-1.06 0L3.22 6.28a.749.749 0 1 1 1.06-1.06L8 8.939l3.72-3.719a.749.749 0 0 1 1.06 0Z"></path></svg>
-          <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-chevron-right Details-content--shown gist-btn-hide"><path d="M6.22 3.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L9.94 8 6.22 4.28a.75.75 0 0 1 0-1.06Z"></path></svg>
+        <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-chevron-down Details-content--hidden"><path d="M12.78 5.22a.749.749 0 0 1 0 1.06l-4.25 4.25a.749.749 0 0 1-1.06 0L3.22 6.28a.749.749 0 1 1 1.06-1.06L8 8.939l3.72-3.719a.749.749 0 0 1 1.06 0Z"></path></svg>
+        <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-chevron-right Details-content--shown gist-btn-hide"><path d="M6.22 3.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L9.94 8 6.22 4.28a.75.75 0 0 1 0-1.06Z"></path></svg>
         `,
         'gist-expand-collapse-btn',
-      );
+        );
       expandCollapseBtn.addEventListener('click', () => {
         const [expandIcon, collapseIcon] = expandCollapseBtn.children;
         const isExpanded = expandCollapseBtn.getAttribute('aria-label') === '折叠文件';
@@ -188,20 +218,24 @@
         expandCollapseBtn.setAttribute('aria-label', isExpanded ? '展开文件' : '折叠文件');
         file.children[1].classList.toggle('gist-file-box-collapsed');
       });
-      fileAction.appendChild(expandCollapseBtn);
+      if (allowExpandCollapseBtnLeftFloat) {
+        // 隐藏方形图标
+        const codeSquare = fileAction.nextElementSibling.firstElementChild;
+        codeSquare.classList.toggle('gist-btn-hide');
+
+        expandCollapseBtn.classList.toggle('gist-expand-collapse-btn-left');
+      }
+      fileAction.insertAdjacentElement('afterend', expandCollapseBtn);
     });
   }
 
   // 初始化折叠/展开全部按钮
   function initExpandCollapseAll() {
     const pageHeadActions = document.querySelector('.pagehead-actions');
-    const expandCollapseAllBtn = createButton('折叠全部', '折叠全部文件', '', 'btn');
+    const expandCollapseAllBtn = createButton('折叠全部', '折叠全部文件', '', 'Button--secondary', 'Button--small', 'Button', 'Button--fullWidth');
     const listItem = document.createElement('li');
     listItem.appendChild(expandCollapseAllBtn);
     pageHeadActions.appendChild(listItem);
-
-    const fileContainers = document.querySelectorAll('.Box-body');
-    const expandCollapseBtns = document.querySelectorAll('.gist-expand-collapse-btn');
 
     expandCollapseAllBtn.addEventListener('click', () => {
       const isExpandedAll = expandCollapseAllBtn.textContent === '折叠全部';
@@ -209,15 +243,14 @@
       expandCollapseAllBtn.textContent = isExpandedAll ? '展开全部' : '折叠全部';
       expandCollapseAllBtn.setAttribute('aria-label', isExpandedAll ? '展开全部文件' : '折叠全部文件');
 
-      expandCollapseBtns.forEach(expandCollapseBtn => {
+      const expandCollapseBtns = document.getElementsByClassName('gist-expand-collapse-btn');
+      [...expandCollapseBtns].forEach(expandCollapseBtn => {
         const [expandIcon, collapseIcon] = expandCollapseBtn.children;
+        const fileContainer = expandCollapseBtn.parentElement.nextElementSibling;
 
         expandIcon.classList.toggle('gist-btn-hide', isExpandedAll);
         collapseIcon.classList.toggle('gist-btn-hide', !isExpandedAll);
         expandCollapseBtn.setAttribute('aria-label', isExpandedAll ? '展开文件' : '折叠文件');
-      });
-
-      fileContainers.forEach(fileContainer => {
         fileContainer.classList.toggle('gist-file-box-collapsed', isExpandedAll);
       });
     });
@@ -225,19 +258,32 @@
 
   // 显示隐藏删除按钮
   function deleteBtnToggle() {
-    const deleteBtn = document.querySelector('.btn-danger.btn-sm.btn');
+    const deleteBtn = document.querySelector('.btn-danger');
     if (deleteBtn) {
       deleteBtn.classList.toggle('gist-btn-hide');
     }
   }
 
+  // 切换展开/折叠按钮位置
+  function toggleExpandCollapseBtnPosition() {
+    const expandCollapseBtns = document.getElementsByClassName('gist-expand-collapse-btn');
+    [...expandCollapseBtns].forEach(expandCollapseBtn => {
+        const codeSquare = expandCollapseBtn.nextElementSibling.firstElementChild;
+        codeSquare.classList.toggle('gist-btn-hide');
+        expandCollapseBtn.classList.toggle('gist-expand-collapse-btn-left');
+      });
+
+    allowExpandCollapseBtnLeftFloat = !allowExpandCollapseBtnLeftFloat;
+    GM_setValue("allowExpandCollapseBtnLeftFloat", allowExpandCollapseBtnLeftFloat);
+  }
+
+  // 初始化
   function init() {
-    const isGistpage = () => /^https:\/\/gist\.github\.com\/[\w-]+\/[a-f0-9]{32}(?:#)?$/.test(location.href);
+    const isGistPage = () => /^https:\/\/gist\.github\.com\/[\w-]+\/[a-f0-9]{32}(?:#)?$/.test(location.href);
 
     const loadButtons = () => {
       initButtons();
       initExpandCollapseAll();
-      deleteBtnToggle();
     }
 
     // 页面跳转后重新加载
@@ -245,9 +291,12 @@
       new window.MutationObserver(mutations => {
         mutations.forEach(mutation => {
           const removedNodes = mutation.removedNodes;
-          if (removedNodes.length > 0 && removedNodes[0].matches('div.turbo-progress-bar') && isGistpage()) {
-            loadButtons();
-            return;
+          if (removedNodes.length > 0 && removedNodes[0].matches('div.turbo-progress-bar')) {
+             if (isGistpage()) {
+               loadButtons();
+             }
+             deleteBtnToggle();
+             return;
           }
         });
       }).observe(document.documentElement, {
@@ -256,9 +305,10 @@
     };
 
     const load = () => {
-      if (isGistpage()) {
-          loadButtons();
+      if (isGistPage()) {
+        loadButtons();
       }
+      deleteBtnToggle();
     }
 
     load();
@@ -267,6 +317,7 @@
 
   // 注册菜单
   GM_registerMenuCommand("显示/隐藏\"删除按钮\"", deleteBtnToggle);
+  GM_registerMenuCommand("切换折叠/展开按钮位置", toggleExpandCollapseBtnPosition);
 
   init();
 })();
